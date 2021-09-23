5794be9df51111abd120b33f9b2d09eb76a93837
jhhong0930 <jhhong0930@gmail.com>
2021-09-23 11:39:22 +0900

    add: service 추가

---
+++ src/main/java/com/sparta/springfirst/service/BoardService.java
@@ -0,0 +1,45 @@
+package com.sparta.springfirst.service;
+
+import com.sparta.springfirst.domain.Board;
+import com.sparta.springfirst.dto.BoardDto;
+
+import java.util.List;
+
+public interface BoardService {
+
+    // 게시글 추가
+    Long register(BoardDto dto);
+
+    // 게시글 목록 조회(페이징 X)
+    List<Board> getList();
+
+    // 게시글 조회
+    BoardDto read(Long id);
+
+    default BoardDto entityToDto(Board entity) {
+
+        BoardDto dto = BoardDto.builder()
+                .id(entity.getId())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+
+        return dto;
+    }
+
+    default Board dtoToEntity(BoardDto dto) {
+
+        Board entity = Board.builder()
+                .id(dto.getId())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+
+        return entity;
+    }
+
+}
---
+++ src/main/java/com/sparta/springfirst/service/BoardServiceImpl.java
@@ -0,0 +1,49 @@
+package com.sparta.springfirst.service;
+
+import com.sparta.springfirst.domain.Board;
+import com.sparta.springfirst.domain.BoardRepository;
+import com.sparta.springfirst.dto.BoardDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService{
+
+    private final BoardRepository repository;
+
+    @Override
+    public Long register(BoardDto dto) {
+
+        log.info("dto = {}", dto);
+
+        Board entity = dtoToEntity(dto);
+
+        log.info("entity = {}", entity);
+
+        repository.save(entity);
+
+        return entity.getId();
+    }
+
+    @Override
+    public List<Board> getList() {
+
+        return repository.findAll(Sort.by(Sort.Direction.DESC, "regdate"));
+    }
+
+    @Override
+    public BoardDto read(Long id) {
+
+        Optional<Board> result = repository.findById(id);
+
+        return result.map(this::entityToDto).orElseThrow(() -> new NullPointerException("존재하지 않는 게시글입니다."));
+    }
+}
